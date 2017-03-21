808b295d093d8c58f2c8dc4462f3773d18290cfd
Brian Shaw <brian@beclicking.com>
2017-03-21 15:39:08 -0500

    code update

--- index.php
+++ index.php
@@ -191,6 +191,11 @@ $(document).ready(function(){
 	   maxFont : 36
 	});
 	
+	$("#div1 #word").flowtype({
+	   minFont : 36,
+	   maxFont : 80
+	});
+	
 	//$("#div1 #definition").css({
 	//	'width': $("#div1 #definition").width(),
 	//	'height': $("#div1 #definition").height()
